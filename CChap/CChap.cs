1bcbc0116b30114504c6e1849aabf7d022234e32
Maria Helena <mariahelenadossantosng@gmail.com>
2023-05-06 18:36:34 -0300

    Meu primeiro commit

---
+++ CChap/CChap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(String[] args) 
+    {
+        // Sangue nos Oioo 🔥
+
+DateTime x = DateTime.Now;
+Console.WriteLine(x);
+DayOfWeek d = x.DayOfWeek;
+Console.WriteLine(d);
+
+    }
+}
---
+++ JavaScript/Program.js
@@ -0,0 +1,32 @@
+
+
+
+// Sangue nos Oioo 🔥
+
+
+let a  = 1.1234;
+let x = a.toFixed(3);
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
